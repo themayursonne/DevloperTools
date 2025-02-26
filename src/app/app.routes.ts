8254a3c4bd21871bc4184f0bda5ef53b65bcfa63
nikitamathews <mail.nikita1611@gmail.com>
2025-02-26 22:41:28 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { JsonEditorComponent } from './json-editor/json-editor.component';
 import { TextUtilitiesComponent } from './text-utilities/text-utilities.component';
 import { CodeBeautifierComponent } from './code-beautifier/code-beautifier.component';
+import { ImageSizeConverterComponent } from './image-size-convertor/image-size-converter.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/json-editor', pathMatch: 'full' },
   { path: 'json-editor', component: JsonEditorComponent },
   { path: 'text-utilities', component: TextUtilitiesComponent },
-  { path: 'code-beautifier', component: CodeBeautifierComponent }
+  { path: 'code-beautifier', component: CodeBeautifierComponent },
+  { path: 'image-size-converter', component: ImageSizeConverterComponent }
 ];
