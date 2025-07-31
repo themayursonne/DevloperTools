582ed3de8be5c31d5d31e5caa1cd6ece7fd894bc
URJIT\uppan <upanshttie@gmail.com>
2025-07-31 09:01:05 +0530

    landingpage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,9 @@ import { JsonEditorComponent } from './json-editor/json-editor.component';
 import { TextUtilitiesComponent } from './text-utilities/text-utilities.component';
 import { CodeBeautifierComponent } from './code-beautifier/code-beautifier.component';
 import { ImageSizeConverterComponent } from './image-size-convertor/image-size-converter.component';
-import { Base64ToolComponent } from './base64-tool/base64-tool.component';
 import { ColumnToRowComponent } from './columntorow/columntorow.component';
+import { Base64ToolComponent } from './base64-tool/base64-tool.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'json-editor', pathMatch: 'full' },
@@ -12,6 +13,8 @@ export const routes: Routes = [
   { path: 'text-utilities', component: TextUtilitiesComponent },
   { path: 'code-beautifier', component: CodeBeautifierComponent },
   { path: 'image-size-converter', component: ImageSizeConverterComponent },
+  { path: 'columntorow', component: ColumnToRowComponent },
+  { path: '', redirectTo: 'base64-tool', pathMatch: 'full' },
   { path: 'base64-tool', component: Base64ToolComponent },
-  { path: 'columntorow', component: ColumnToRowComponent }
+  { path: '**', redirectTo: 'base64-tool' } 
 ];
