d99afccecdafa1c958f5d3281d392f1bba873a1f
URJIT\uppan <upanshttie@gmail.com>
2025-07-31 09:14:36 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,20 +6,13 @@ import { ImageSizeConverterComponent } from './image-size-convertor/image-size-c
 import { ColumnToRowComponent } from './columntorow/columntorow.component';
 import { Base64ToolComponent } from './base64-tool/base64-tool.component';
 
-
 export const routes: Routes = [
-  { path: '', redirectTo: 'json-editor', pathMatch: 'full' },
+  { path: '', redirectTo: 'base64-tool', pathMatch: 'full' }, // default route
   { path: 'json-editor', component: JsonEditorComponent },
   { path: 'text-utilities', component: TextUtilitiesComponent },
   { path: 'code-beautifier', component: CodeBeautifierComponent },
   { path: 'image-size-converter', component: ImageSizeConverterComponent },
   { path: 'columntorow', component: ColumnToRowComponent },
-  { path: '', redirectTo: 'base64-tool', pathMatch: 'full' },
   { path: 'base64-tool', component: Base64ToolComponent },
-<<<<<<< HEAD
-  { path: '**', redirectTo: 'base64-tool' } 
-=======
-  { path: 'columntorow', component: ColumnToRowComponent },
-  { path: '**', redirectTo: 'json-editor' }
->>>>>>> 0b92b67c01e79391a3e4bcd29e938937ef764443
+  { path: '**', redirectTo: 'base64-tool' } // wildcard route
 ];
